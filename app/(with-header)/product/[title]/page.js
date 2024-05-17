3d533ba3236309e99c7c4540654b171e44b09357
Priyansh Rai <priyanshrai16@gmail.com>
2024-05-17 11:22:51 +0530

    Add BreadcrumbJsonLd for product page

--- app/(with-header)/product/[title]/page.js
+++ app/(with-header)/product/[title]/page.js
@@ -1,7 +1,7 @@
+import { BreadcrumbJsonLd } from "next-seo";
 import RoomPage from "../MainPage";
-import axios from "axios";
 
-export async function generateMetadata({params}) {
+export async function generateMetadata({ params }) {
   // let url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/getSingleProduct?id=`;
 
   // const response = await axios.get(`${url}${params.id}`);
@@ -15,7 +15,27 @@ export async function generateMetadata({params}) {
 const page = () => {
   return (
     <>
-      <RoomPage  />
+      <BreadcrumbJsonLd
+        useAppDir={true}
+        itemListElements={[
+          {
+            position: 1,
+            name: "Home",
+            item: "https://www.ayatrio.com",
+          },
+          {
+            position: 2,
+            name: "Product",
+            item: "https://www.ayatrio.com/product",
+          },
+          {
+            position: 3,
+            name: params.title,
+            item: `https://www.ayatrio.com/product/${params.title}`,
+          },
+        ]}
+      />
+      <RoomPage />
     </>
   );
 };
