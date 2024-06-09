af9e2330bb1f3b629a3f887e98fdf6489edd4f16
Mkaif-Qureshi <quremkaif7@gmail.com>
2024-06-09 17:23:14 +0530

    a fix

--- components/FAQ/FAQ.jsx
+++ components/FAQ/FAQ.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 import React from "react";
 import { FAQs } from "./FAQData";
 import dynamic from "next/dynamic";
--- components/FAQ/QuestionAnswer.jsx
+++ components/FAQ/QuestionAnswer.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from "react";
 
 const QuestionAnswer = ({ question, answer }) => {
