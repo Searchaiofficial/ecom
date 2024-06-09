6e544c997bbd2d629d7f54c506fe9ba346dd8bf8
Mkaif-Qureshi <quremkaif7@gmail.com>
2024-06-09 16:35:49 +0530

    added the svgs

--- components/FAQ/QuestionAnswer.jsx
+++ components/FAQ/QuestionAnswer.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { FaAngleUp, FaAngleDown } from "react-icons/fa";
 
 const QuestionAnswer = ({ question, answer }) => {
   const [show, setShow] = useState(false);
@@ -9,7 +8,7 @@ const QuestionAnswer = ({ question, answer }) => {
       <hr />
       <div className="flex mt-[30px] p-5 cursor-pointer" onClick={() => setShow(!show)}>
         <h3 className="font-semibold text-lg">{question}</h3>
-        {show ? <FaAngleUp className="ml-auto mt-1 text-gray-600" /> : <FaAngleDown className="ml-auto mt-1 text-gray-600" />}
+        {show ? <img src="/icon/up-arrow.svg" alt="Up Arrow" className="ml-auto mt-1 w-4 h-4 text-gray-600" /> : <img src="/icon/down-arrow.svg" alt="Down Arrow" className="ml-auto mt-1 w-4 h-4 text-gray-600" />}
       </div>
       {show && <p className="p-6 text-base font-normal leading-7 text-gray-800">{answer}</p>}
     </div>
