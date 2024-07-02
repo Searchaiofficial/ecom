8ed4fcd1fd714e16169cc8c856a865480474ee1f
Priyansh Rai <priyanshrai16@gmail.com>
2024-07-02 21:35:21 +0530

    Add generateMetadata function to /suggestion/:id page to generate dynamic metadata

--- app/(with-header)/suggestion/[id]/page.jsx
+++ app/(with-header)/suggestion/[id]/page.jsx
@@ -1,4 +1,14 @@
+import { fetchSuggestionData } from "@/components/Features/api";
 import Suggestion from "@/components/suggestion/Suggestion";
+
+export const generateMetadata = async ({ params: { id } }) => {
+  const suggestion = await fetchSuggestionData(id);
+
+  return {
+    title: suggestion?.metadata?.title,
+    description: suggestion.summary,
+  };
+};
 
 const SuggestionPage = async ({ params: { id } }) => {
   return <Suggestion id={id} />;
