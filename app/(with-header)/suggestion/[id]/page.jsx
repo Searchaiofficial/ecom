cd11edb524900a8555c1a55b685c2df261fea423
Priyansh Rai <priyanshrai16@gmail.com>
2024-07-02 22:23:06 +0530

    Add metadata and JSON-LD for SEO in suggestion page

--- app/(with-header)/suggestion/[id]/page.jsx
+++ app/(with-header)/suggestion/[id]/page.jsx
@@ -1,5 +1,6 @@
 import { fetchSuggestionData } from "@/components/Features/api";
 import Suggestion from "@/components/suggestion/Suggestion";
+import { ArticleJsonLd } from "next-seo";
 
 export const generateMetadata = async ({ params: { id } }) => {
   const suggestion = await fetchSuggestionData(id);
@@ -7,11 +8,41 @@ export const generateMetadata = async ({ params: { id } }) => {
   return {
     title: suggestion?.metadata?.title,
     description: suggestion.summary,
+    openGraph: {
+      title: suggestion?.metadata?.title,
+      description: suggestion.summary,
+      images: [
+        {
+          url: suggestion.mainImage,
+        },
+        {
+          url: suggestion.suggestionCardImage,
+        },
+      ],
+    },
   };
 };
 
 const SuggestionPage = async ({ params: { id } }) => {
-  return <Suggestion id={id} />;
+  const suggestion = await fetchSuggestionData(id);
+
+  return (
+    <>
+      {!!suggestion ? (
+        <ArticleJsonLd
+          useAppDir={true}
+          type="BlogPosting"
+          title={suggestion.metadata.title}
+          description={suggestion.summary}
+          images={[suggestion.mainImage, suggestion.suggestionCardImage]}
+          datePublished={suggestion.createdAt?.toString()}
+          dateModified={suggestion.updatedAt?.toString()}
+          authorName={suggestion.author?.name || "Ayatrio"}
+        />
+      ) : null}
+      <Suggestion id={id} />;
+    </>
+  );
 };
 
 export default SuggestionPage;
