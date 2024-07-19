22f726d5e2611154e48b4c1f98a23695064417b4
Priyansh Rai <priyanshrai16@gmail.com>
2024-07-19 17:23:34 +0530

    Add Google Tag Manager initialization script

--- app/(with-header)/page.js
+++ app/(with-header)/page.js
@@ -13,7 +13,9 @@ import SaveDeviceIdLocalstorage from "@/utils/SaveDeviceIdLocalstorage ";
 
 import { Suspense } from "react";
 // import ChatPrompt from "../../components/ChatPromptWidget/chatprompt";
-const ChatPrompt = dynamic(() => import("../../components/ChatPromptWidget/chatprompt"));
+const ChatPrompt = dynamic(() =>
+  import("../../components/ChatPromptWidget/chatprompt")
+);
 import SaveUserCoordinatesOnscroll from "@/utils/SaveUserCoordinatesOnScroll";
 import { OrganizationJsonLd, SiteLinksSearchBoxJsonLd } from "next-seo";
 
--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import { GoogleTagManager } from "@next/third-parties/google";
 import FooterWrapper from "@/components/FooterWrapper/FooterWrapper";
 import NextTopLoader from "nextjs-toploader";
 import { BASE_URL } from "@/constants/base-url";
+import Script from "next/script";
 // const myFont = localFont({
 //   src: "../public/Font/Jost-Regular.ttf",
 // });
@@ -32,7 +33,7 @@ export const metadata = {
         url: "https://ayatrio.com/api/og",
         width: 1200,
         height: 630,
-        rel: "preload"
+        rel: "preload",
       },
     ],
   },
@@ -63,6 +64,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={poppins.className}>
       <GoogleTagManager gtmId={gtmId} />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-46J3H63RLT`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-46J3H63RLT');
+          `,
+        }}
+      />
       <body>
         <Providers>
           <NextTopLoader color="#000" showSpinner={false} zIndex={99999} />
