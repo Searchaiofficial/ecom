8cfa4d4f8b174e3a39b64eea05b72b0e9ce11c22
Priyansh Rai <priyanshrai16@gmail.com>
2024-08-05 01:29:05 +0530

    Revert "Migrate to swiper web components in dataslider component"
    This reverts commit e86a48f3ba4a6c6da3d026175b04fa39d72bc2b8.

--- components/Cards/Dataslider.jsx
+++ components/Cards/Dataslider.jsx
@@ -1,18 +1,32 @@
 import Card from "./card";
 import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
-import { register } from "swiper/element/bundle";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "./styles.css";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/free-mode";
 import "swiper/css/mousewheel";
 import "swiper/css/scrollbar";
+import { Pagination, Scrollbar, Mousewheel, FreeMode } from "swiper/modules";
 import { viewItemList } from "@/tag-manager/events/view_item_list";
 
 const Dataslider = ({ category, data, sliderIndex }) => {
   const swiperRef = useRef(null);
   const [isPopupVisible, setPopupVisible] = useState(false);
+  const swiperOptions = {
+    slidesPerView: 3,
+    centeredSlides: false,
+    spaceBetween: 5,
+    modules: [Pagination, Scrollbar, Mousewheel, FreeMode],
+    navigation: {
+      nextEl: `.custom-next-button-${sliderIndex}`,
+      prevEl: `.custom-prev-button-${sliderIndex}`,
+    },
+    noSwiping: true,
+    allowSlidePrev: true,
+    allowSlideNext: true,
+  };
 
   const [productData, setProductData] = useState([]);
   useEffect(() => {
@@ -21,48 +35,6 @@ const Dataslider = ({ category, data, sliderIndex }) => {
       setProductData(Data);
     }
   }, [data]);
-
-  useEffect(() => {
-    register();
-
-    const params = {
-      centeredSlides: false,
-      spaceBetween: 10,
-      noSwiping: true,
-      scrollbar: {
-        el: `.swiper-scrollbar-dataslider-${sliderIndex}`,
-        draggable: true,
-      },
-      mousewheel: {
-        forceToAxis: true,
-        invert: false,
-      },
-      breakpoints: {
-        300: {
-          slidesPerView: 1.2,
-          spaceBetween: 10,
-        },
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        1024: {
-          slidesPerView: 4,
-          spaceBetween: 10,
-        },
-      },
-      allowSlidePrev: true,
-      allowSlideNext: true,
-      navigation: {
-        nextEl: `.custom-next-button-${sliderIndex}`,
-        prevEl: `.custom-prev-button-${sliderIndex}`,
-      },
-    };
-
-    Object.assign(swiperRef.current, params);
-
-    swiperRef.current.initialize();
-  }, []);
 
   useEffect(() => {
     if (productData.length > 0) {
@@ -89,6 +61,7 @@ const Dataslider = ({ category, data, sliderIndex }) => {
           </h2>
           <div className="Slidenav flex text-2xl cursor-pointer text-white rounded-full gap-2">
             <div
+              onClick={() => swiperRef.current.swiper.slidePrev()}
               className={`custom-prev-button-${sliderIndex} hover:bg-400 hover:scale-110 hover:text-slate-100 pr-6`}
             >
               <Image
@@ -101,6 +74,7 @@ const Dataslider = ({ category, data, sliderIndex }) => {
               />
             </div>
             <div
+              onClick={() => swiperRef.current.swiper.slideNext()}
               className={`custom-next-button-${sliderIndex} hover:bg-400 sm:translate-y-0 translate-y-10 hover:scale-110 hover:text-slate-100`}
             >
               <Image
@@ -114,9 +88,32 @@ const Dataslider = ({ category, data, sliderIndex }) => {
             </div>
           </div>
         </div>{" "}
-        <swiper-container
-          init="false"
+        <Swiper
+          scrollbar={{
+            hide: false,
+            draggable: true,
+          }}
+          mousewheel={{
+            forceToAxis: true,
+            invert: false,
+          }}
+          breakpoints={{
+            300: {
+              slidesPerView: 1.2,
+              spaceBetween: 5,
+            },
+
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 5,
+            },
+          }}
+          allowSlidePrev={true}
+          allowSlideNext={true}
+          slideNextClass={`custom-next-button-${sliderIndex}`}
+          slidePrevClass={`custom-prev-button-${sliderIndex}`}
           ref={swiperRef}
+          {...swiperOptions}
           className="mySwiper pl-5 overflow-x-auto"
         >
           {!productData ? (
@@ -125,7 +122,7 @@ const Dataslider = ({ category, data, sliderIndex }) => {
             </div>
           ) : (
             productData.map((product) => (
-              <swiper-slide key={product._id}>
+              <SwiperSlide key={product._id}>
                 <div className="grid grid-cols-1 mt-2 h-full fade-in ">
                   <Card
                     cardkey={product._id}
@@ -149,13 +146,10 @@ const Dataslider = ({ category, data, sliderIndex }) => {
                     urgency={product.urgency}
                   />
                 </div>
-              </swiper-slide>
+              </SwiperSlide>
             ))
           )}
-        </swiper-container>
-        <div
-          className={`swiper-scrollbar-dataslider-${sliderIndex} h-[2px]`}
-        ></div>
+        </Swiper>
         {/* <div className="">
           {itm1.map((item) => (
             <div key="item.label._id" className="flex flex-row gap-5">
