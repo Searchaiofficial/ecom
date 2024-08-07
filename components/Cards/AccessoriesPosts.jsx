44ab19c36eb8161e541447681a2676a4d4d06055
Priyansh Rai <priyanshrai16@gmail.com>
2024-08-07 18:48:25 +0530

    Remove unused code

--- components/Cards/AccessoriesPosts.jsx
+++ components/Cards/AccessoriesPosts.jsx
@@ -1,30 +1,6 @@
-import { useRef } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Scrollbar, Mousewheel, FreeMode } from "swiper/modules";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import "swiper/css/free-mode";
-import "swiper/css/mousewheel";
-import "swiper/css/scrollbar";
-import Card from "../../components/Cards/card";
 import AccessoriesSlider from "./AccessoriesSlider";
 
 const AccessoriesPosts = ({ accessories }) => {
-  const swiper1Ref = useRef(null);
-
-  const swiperOptions = {
-    centeredSlides: false,
-    spaceBetween: 1,
-    modules: [Pagination, Scrollbar, Mousewheel, FreeMode],
-    navigation: {
-      nextEl: ".custom-next-button",
-      prevEl: ".custom-prev-button",
-    },
-    noSwiping: true,
-    allowSlidePrev: true,
-    allowSlideNext: true,
-  };
-
   return (
     <div>
       {accessories && accessories.length > 0 && (
