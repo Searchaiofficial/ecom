2b262e21f0f74952000d4d71d74a56c026003069
Priyansh Rai <priyanshrai16@gmail.com>
2024-08-07 20:57:00 +0530

    Refactor BlogRelatedProducts component and add BlogRelatedProductsSlider component

--- components/Cards/BlogRelatedProducts.jsx
+++ components/Cards/BlogRelatedProducts.jsx
@@ -1,46 +1,18 @@
-import Card from "./card";
-import Image from "next/image";
-import React, { useEffect, useRef, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { useRef, useState } from "react";
 import PopUp from "../Reviews/PopUp";
 import "./styles.css";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import "swiper/css/free-mode";
-import "swiper/css/mousewheel";
-import "swiper/css/scrollbar";
-import {
-  Pagination,
-  Scrollbar,
-  Mousewheel,
-  FreeMode,
-} from "swiper/modules";
-// import axios from "axios";;
+import BlogRelatedProductsSlider from "./BlogRelatedProductsSlider";
 
 const BlogRecommendedProducts = ({ relatedProducts, title }) => {
   const [isPopupVisible, setPopupVisible] = useState(false);
 
-  const swiperOptions2 = {
-    slidesPerView: 4.08,
-    centeredSlides: false,
-    spaceBetween: 5,
-    modules: [Pagination, Scrollbar, Mousewheel, FreeMode],
-    navigation: {
-      nextEl: ".custom-next-button",
-      prevEl: ".custom-prev-button",
-    },
-    noSwiping: true,
-    allowSlidePrev: true,
-    allowSlideNext: true,
-  };
   const closePopup = () => {
     setPopupVisible(false);
   };
   const swiper1Ref = useRef(null);
 
   const filteredProducts = relatedProducts?.filter(
-    (product) => product.subcategory !== 'Accessories'
+    (product) => product.subcategory !== "Accessories"
   );
 
   return (
@@ -48,9 +20,7 @@ const BlogRecommendedProducts = ({ relatedProducts, title }) => {
       <div className=" mt-20  bg-white ">
         <div className="mb-2 w-full flex justify-between items-center">
           <h2 className="font-semibold text-2xl py-[15px]">
-            {relatedProducts && relatedProducts.length === 0
-              ? ""
-              : title}
+            {relatedProducts && relatedProducts.length === 0 ? "" : title}
           </h2>
           <div className="Slidenav flex  bg-white text-2xl cursor-pointer  text-white rounded-full gap-2">
             <div
@@ -64,80 +34,7 @@ const BlogRecommendedProducts = ({ relatedProducts, title }) => {
           </div>
         </div>
         <PopUp isPopupVisible={isPopupVisible} closePopup={closePopup} />
-        <Swiper
-          ref={swiper1Ref}
-          {...swiperOptions2}
-          scrollbar={{
-            hide: false,
-            draggable: true,
-          }}
-          mousewheel={{
-            forceToAxis: true,
-            invert: false,
-          }}
-          freeMode={{
-            enabled: false,
-            sticky: true,
-          }}
-          breakpoints={{
-            300: {
-              slidesPerView: 1.2,
-              spaceBetween: 10,
-            },
-
-            640: {
-              slidesPerView: 2,
-              spaceBetween: 10,
-            },
-            1024: {
-              slidesPerView: 4,
-              spaceBetween: 10,
-            },
-          }}
-          allowSlideNext={true}
-          allowSlidePrev={true}
-          slideNextClass="custom-next-button"
-          slidePrevClass="custom-prev-button"
-          className="px-10"
-        >
-          {!filteredProducts ? (
-            <SwiperSlide>
-              <div className="flex"></div>
-            </SwiperSlide>
-          ) : (
-            filteredProducts.map((product, idx) => {
-              return (
-                <SwiperSlide key={idx} className="ml-0">
-                  <div className="grid grid-cols-1 w-full h-full fade-in ">
-                    <Card
-                      title={product.productTitle}
-                      productImages={product.productImages}
-                      specialPrice={product?.specialprice}
-                      price={product.perUnitPrice}
-                      desc={product.productTitle}
-                      shortDescription={product.shortDescription}
-                      demandtype={product.demandtype}
-                      imgSrc={product.images}
-                      rating={product.ratings}
-                      key={idx}
-                      id={product._id}
-                      category={product.category}
-                      productId={product.productId}
-                      setPopupVisible={setPopupVisible}
-                      cssClass={"card1flex"}
-                      // inCart={inCart}
-                      unitType={product.unitType}
-                      productType={product.productType}
-                      expectedDelivery={product.expectedDelivery}
-                      discountedprice={product.discountedprice}
-                      offer={product.offer}
-                    />
-                  </div>
-                </SwiperSlide>
-              );
-            })
-          )}
-        </Swiper>
+        <BlogRelatedProductsSlider data={filteredProducts} />
       </div>
     </div>
   );
