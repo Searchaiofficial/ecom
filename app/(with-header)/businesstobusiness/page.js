c38b2a6ba43568537a01ac9c707d1321ee9b47b2
Harsh Kumar <harshkumar878983@gmail.com>
2024-08-08 17:00:51 +0530

    Some bugs fixed

--- app/(with-header)/businesstobusiness/page.js
+++ app/(with-header)/businesstobusiness/page.js
@@ -1,16 +1,15 @@
-// import Commercial from "@/Components/Business/Commercial/page";
-import Commercial from "@/components/Business/Commercial/page";
-import Home from "@/Components/Business/HomePage/page";
-import Nav from "@/Components/Business/Nav/page";
-import Projects from "@/Components/Business/Projects/Projects";
-import Purchasing from "@/Components/Business/Purchasing/page";
-import Recomend from "@/Components/Business/Recomend/page";
-import Service from "@/Components/Business/Service/page";
-import Tips from "@/Components/Business/Tips/page";
+import Commercial from "@/components/Business/Commercial";
+import Home from "@/components/Business/HomePage";
+import Nav from "@/components/Business/Nav";
+import Projects from "@/components/Business/Projects";
+import Purchasing from "@/components/Business/Purchasing";
+import Recomend from "@/components/Business/Recomend";
+import Service from "@/components/Business/Service";
+import Tips from "@/components/Business/Tips";
 
 const page = () => {
   return (
-    <div className="animate-fade-in">
+    <div>
       <Nav />
       <Home />
       <Recomend />
